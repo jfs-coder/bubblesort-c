bac66f97e23eec82cbdd37e856bfe6452bc06e7f
jfs-coder <jfs-coder@protonmail.com>
2020-12-04 04:51:54 +0000

    made my own version without looking at example code. took a while to figure it out

---
+++ mybubble.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+
+	int a, b, c, swap;
+
+	int arr[10] = {5,8,2,9,4,6,1,7,3,0};
+
+	for(a = 0; a < 10; a++) {
+		printf("Number %d: \n", arr[a]);
+	}
+
+	printf("---SEPARATOR---\n");
+
+	
+	for (a = 0; a < 10; a++) {
+		for (b = 0; b < 9; b++) {
+			if(arr[b] > arr[b+1]) {
+				swap = arr[b];
+				arr[b] = arr[b+1];
+				arr[b+1] = swap;
+			}
+		}
+	}
+
+	for(a = 0; a < 10; a++) {
+		printf("Number %d: \n", arr[a]);
+	}
+
+	return 0;
+}
+
+
