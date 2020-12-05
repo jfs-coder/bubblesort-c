fc8922e03b57257944121193b016e776e065d13c
jfs-coder <jfs-coder@protonmail.com>
2020-12-05 00:54:40 +0000

    different way of printing output

--- bubblesort.c
+++ bubblesort.c
@@ -6,26 +6,30 @@ int main() {
 
 	int a, b, c, swap;
 
-	int arr[10] = {5,8,2,9,4,6,1,7,3,0};
+	int tot = 5;
 
-	for(a = 0; a < 10; a++) {
+	int arr[5] = {5,8,2,9,7};
+
+	for(a = 0; a < tot; a++) {
 		printf("%d ", arr[a]);
 	}
 
 	printf("\n");
 	
-	for (a = 0; a < 10; a++) {
-		for (b = 0; b < 9; b++) {
+	for (a = 0; a < tot; a++) {
+		for (b = 0; b < tot-1; b++) {
+			printf("Is %d larger than %d?\n", arr[b], arr[b+1]);
 			if(arr[b] > arr[b+1]) {
+				printf("Yes, swaping %d -> %d\n", arr[b], arr[b+1]);
 				swap = arr[b];
 				arr[b] = arr[b+1];
 				arr[b+1] = swap;
 			}
+			for(c = 0; c < tot; c++) {
+				printf("%d ", arr[c]);
+			}
+			printf("\n");
 		}
-		for(c = 0; c < 10; c++) {
-			printf("%d ", arr[c]);
-		}
-		printf("\n");
 	}
 
 	return 0;
