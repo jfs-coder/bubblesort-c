34bd5a1a7ca570092d9f90d78bbda3e625e0577c
jfs-coder <jfs-coder@protonmail.com>
2020-12-05 05:16:39 +0000

    added #define for TOTAL

--- bubblesort.c
+++ bubblesort.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#define TOTAL 20
 
 int main() {
 
@@ -14,25 +15,21 @@ int main() {
 
 	swap = 0; // has a swap occured in an instance? we'll use this to exit faster.
 
-	int tot = 20;
-
-	// int arr[10] = {5,8,2,9,7,3,0,4,1,6};
-	int arr[20] = {1, 18, 13, 5, 12, 6, 17, 2, 8, 16, 3, 19, 4, 20, 7, 9, 10, 14, 11, 15};
-
+	int arr[TOTAL] = {1, 18, 13, 5, 12, 6, 17, 2, 8, 16, 3, 19, 4, 20, 7, 9, 10, 14, 11, 15};
 
 	printf("\n\n          [JFS BUBBLESORT VERSION %s]\n\n", VERSION);
 
-	for (a = 0; a < tot; a++) {
+	for (a = 0; a < TOTAL; a++) {
 		swap = 0;
-		for (b = 0; b < tot-1; b++) {
-			printf("_         Iteration#: %02d  [%02d > %02d] ", a, arr[b], arr[b+1]);
+		for (b = 0; b < TOTAL-1; b++) {
+			printf("          Iteration#: %02d  [%02d > %02d] ", a, arr[b], arr[b+1]);
 			if(arr[b] > arr[b+1]) {
 				temp = arr[b];
 				arr[b] = arr[b+1];
 				arr[b+1] = temp;
 				swap = 1; // a swap has occured.
 			}
-			for(c = 0; c < tot; c++) {
+			for(c = 0; c < TOTAL; c++) {
 				printf("%02d ", arr[c]);
 			}
 			printf("\r");
@@ -41,7 +38,7 @@ int main() {
 		}
 		// if no swap has happened, you can exit
 		if (!swap) { 
-			printf("\nNo swap iappened! Exiting early. Ended on iteration # %d instead of %d!\n", a, tot);
+			printf("\n\n          No swap happened! Exiting early. Ended on iteration # %02d instead of %02d!\n\n\n", a, TOTAL);
 			return 0;
 		}
 	}
